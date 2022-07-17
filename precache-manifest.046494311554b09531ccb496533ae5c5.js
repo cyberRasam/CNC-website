74a92d4f2355b0b5da1607ac71c78ac0baa0f73a
cyberrasam <you@example.com>
2022-07-17 18:38:18 +0300

    Updates

---
+++ precache-manifest.046494311554b09531ccb496533ae5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3932159845375f32bb62",
+    "url": "/CNC-website/static/css/main.07274960.chunk.css"
+  },
+  {
+    "revision": "3932159845375f32bb62",
+    "url": "/CNC-website/static/js/main.b257bbf3.chunk.js"
+  },
+  {
+    "revision": "1ea2f723b3c8a4192673",
+    "url": "/CNC-website/static/js/runtime~main.ae007494.js"
+  },
+  {
+    "revision": "ab70f77b57756b8d1c48",
+    "url": "/CNC-website/static/js/2.86d5e624.chunk.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/CNC-website/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "eebfa8bc204b6e95666f1d84aecfee9b",
+    "url": "/CNC-website/index.html"
+  }
+];
