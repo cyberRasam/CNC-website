7bbb4a738d434c5278519da825619b6d1c46741e
cyberRasam <75702946+cyberRasam@users.noreply.github.com>
2022-07-27 19:11:06 +0300

    ordering process section completion

--- src/components/process/Process.jsx
+++ src/components/process/Process.jsx
@@ -2,7 +2,51 @@ import React from 'react';
 import './process.css';
 
 const Process = () => {
-  return <div></div>;
+  return (
+    <div className="cncium_process gradient__section">
+      <h2 className="cncium_process_header">Our ordering process</h2>
+      <div className="cncium_process_box">
+        <div className="cncium_process_items">
+          <div className="cncium_process_items-order">
+            <div className="circle"></div>
+            <div className="cncium_process_items-order-mobile">
+              <h4>Contact us</h4>
+              <p>Fill the following contact form</p>
+            </div>
+          </div>
+          <div className="cncium_process_items-order">
+            <div className="circle"></div>
+            <div className="cncium_process_items-order-mobile">
+              <h4>Get our estimated quote</h4>
+              <p>
+                Confirm the specs of the parts and select a lead time that suits
+                your schedule
+              </p>
+            </div>
+          </div>
+          <div className="cncium_process_items-order">
+            <div className="circle"></div>
+            <div className="cncium_process_items-order-mobile">
+              <h4>Production</h4>
+              <p>
+                We select the best manufacturer for your order, and production
+                begins
+              </p>
+            </div>
+          </div>
+          <div className="cncium_process_items-order">
+            <div className="circle"></div>
+            <div className="cncium_process_items-order-mobile">
+              <h4>Delivery</h4>
+            </div>
+            <button type="button" className="cncium_process_contact">
+              Contact us
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Process;
